eea82e8cd6d4bedd245dffa250138d725e145938
Richard Chen <richard.c.chen@gmail.com>
2018-06-25 00:27:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 
 function deleteFromObjectByKey(object, key){
  var newobject = Object.assign({}, object);
- delete newobject.key;
+ delete newobject[key];
  return newobject
 }
